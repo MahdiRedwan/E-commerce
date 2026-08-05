7d7624672c52458b8ace989e2b7223e5987393ae
MahdiRedwan <mahdi.redwan@northsouth.edu>
2026-08-06 03:10:16 +0600

    Fix Stripe API version

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPESECRETKEY!, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: '2026-07-29.dahlia',
 })
 
 export async function POST(request: Request) {
